ae4da86699d215a9ce81c5258fe5114cfc7a847a
haupham <hau.pham@hdwebsoft.co>
2021-01-19 23:30:26 +0700

    Login page

--- src/view/InterfaceUser/container.tsx
+++ src/view/InterfaceUser/container.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Carousel from './Carousel';
 import ListMovie from './ListMovie';
 import HomeTools from './HomeTools';
+import ReactNotification from 'react-notifications-component'
  function container() {
   return (
     <div>
        <Carousel />
-       <ListMovie />
        <HomeTools />
+       <ListMovie />
+       <ReactNotification />
     </div>
   )
 }
